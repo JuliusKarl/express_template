c4cf73ad00daef7b5ddec2942a4146a2860bc48e
Julius Macrohon <julius.karl.macrohon@gmail.com>
2020-03-10 13:13:24 +1300

    Laptop Update

--- app.js
+++ app.js
@@ -14,14 +14,14 @@ app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', '*');
-    if (req.method == 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, PATCH, POST, DELETE, GET');
-        return res.status(200).json({});
-    };
-});
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Headers', '*');
+//     if (req.method == 'OPTIONS') {
+//         res.header('Access-Control-Allow-Methods', 'PUT, PATCH, POST, DELETE, GET');
+//         return res.status(200).json({});
+//     };
+// });
 
 
 
